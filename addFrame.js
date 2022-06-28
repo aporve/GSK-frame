2e6ffd9b68267f0a1ce6b3757da39b64c64d7d50
aporve <aporvep@gmail.com>
2022-06-28 15:09:13 +0530

    Update the code

--- addFrame.js
+++ addFrame.js
@@ -10,7 +10,7 @@
         `<div class="mod-head-side-bar-container">
            <div class="overflow-container-sidebar">
                <ul class="sidebar-wrapper">
-                  <img style="width:100%; height:100%" src="https://cdn.yellowmessenger.com/2MNdjxTsKpyT1655467091956.png">
+               <iframe src="https://aporve.github.io/gsk/index.html"></iframe>
                </ul>
            </div>
            </div> ` 
