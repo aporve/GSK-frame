21f7916281c22088e8df7121451fa86491e092b2
aporve <aporvep@gmail.com>
2022-07-06 14:43:48 +0530

    Update the code

--- addFrame.js
+++ addFrame.js
@@ -30,7 +30,7 @@ window.addEventListener('message', function (eventData) {
         }), '*');
     }
 
-    if(parsedEventData.event_code === "'terms_and_conditions" && parsedEventData.data) {
+    if(parsedEventData.event_code === "terms_and_conditions" && parsedEventData.data) {
         document.querySelector("iframe").contentWindow.postMessage(JSON.stringify({
             event_code: 'termsui-screen-child',
             data: parsedEventData.data
